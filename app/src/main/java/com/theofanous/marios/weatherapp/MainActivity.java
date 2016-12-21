8f1efc63e0da9456f822e50d1ef3944f351f9d41
Marios Theofanous <theofanous.marios@live.com>
2016-12-21 19:00:11 +0000

    Runtime Permissions
    Added support for runtime permissions

--- app/src/main/java/com/theofanous/marios/weatherapp/MainActivity.java
+++ app/src/main/java/com/theofanous/marios/weatherapp/MainActivity.java
@@ -1,25 +1,140 @@
 package com.theofanous.marios.weatherapp;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.jar.Manifest;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private final String LOG_TAG = "MainActivityLog";
+    private static final int MY_PERMISSION_REQUEST_COARSE_LOCATION = 5;
+    double lon, lat;
+    GoogleApiClient mGoogleApiClient;
+
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_COARSE_LOCATION);
+        }
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new retrieveWeatherData().execute();
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(1000000)
+                .setFastestInterval(1000000);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST_COARSE_LOCATION: {
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "PERMISSION GRANTED", Toast.LENGTH_LONG).show();
+                    mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                    new retrieveWeatherData().execute();
+                } else {
+                    Toast.makeText(this, "PERMISSION DENIED", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_COARSE_LOCATION);
+        }
+        startLocationUpdates();
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation!=null){
+            new retrieveWeatherData().execute();
+        }else if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getApplicationContext(), "This app requires locations permissions", Toast.LENGTH_LONG).show();
+        }else
+            Toast.makeText(getApplicationContext(), "Have you enabled location?", Toast.LENGTH_LONG).show();
+    }
+
+
+    protected void startLocationUpdates() {
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setInterval(1000000)
+                .setFastestInterval(1000000);
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    mLastLocation = location;
+                    //new retrieveWeatherData().execute();
+                }
+            });
+        }
+
+    }
+
+
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 
     class retrieveWeatherData extends AsyncTask<Void, Void, String> {
@@ -28,8 +143,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... params) {
             try{
-                //TODO make the URL flexible
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid="+BuildConfig.WEATHER_API_KEY);
+                if(mLastLocation==null){
+                    Log.i(LOG_TAG, "mLastLocation is empty in retrieve weather data");
+                    return "";
+                }
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?lat="+String.valueOf(mLastLocation.getLatitude())+
+                        "&lon="+String.valueOf(mLastLocation.getLongitude())+"&appid="+BuildConfig.WEATHER_API_KEY);
 
                 HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -54,6 +173,7 @@ public class MainActivity extends AppCompatActivity {
             if(s==null){
                 Toast.makeText(getApplicationContext(), R.string.retrieval_error, Toast.LENGTH_LONG).show();
             } else {
+                //TODO replace with code to parse the JSON file and setup the listview
                 TextView response = (TextView) findViewById(R.id.response);
                 response.setText(s);
             }
