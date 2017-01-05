61427dacb3c090d3a1187e1d954f4a2e70691375
Marios Theofanous <theofanous.marios@live.com>
2017-01-05 20:05:37 +0000

    *Updated missing files*

---
+++ app/src/main/java/com/theofanous/marios/weatherapp/About.java
@@ -0,0 +1,32 @@
+package com.theofanous.marios.weatherapp;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.about_toolbar);
+        toolbar.setTitle(getString(R.string.about_title));
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView first = (TextView) findViewById(R.id.about_first);
+
+        String firstStr = getString(R.string.designed_by)+
+                " Nikita Golubev " +
+                getString(R.string.and)+
+                " EpicCoders " +
+                getString(R.string.from)+" "+
+                getString(R.string.flaticon);
+
+        first.setText(firstStr);
+    }
+}
